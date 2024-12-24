96a2cc09772e2b49e4a51ad5bc2f3cdac567199c
Lafi007 <141007452+Lafi007@users.noreply.github.com>
2024-12-24 16:37:12 +0300

    Update bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,10 @@
 # bounce.py
 #
 # Exercise 1.5
+bouncinghight=100
+bounc_num=1
+while bouncinghight>0:
+    bouncinghight=bouncinghight*3/5
+    bounc_num=bounce_num+1
+    print ("bounce number:",bounce_num,"Ball Hight is:",bouncinghight)
+  
